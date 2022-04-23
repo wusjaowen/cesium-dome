3ed481ddce91eebf2230425981bc05f35741ac05
loveYOU2020 <1019941431@qq.com>
2022-04-23 23:08:09 +0800

    3DTileset加载模型 裁剪

--- src/page/login/index_3dtit.vue
+++ src/page/login/index_3dtit.vue
@@ -43,35 +43,36 @@ export default {
 
     function loadTileset(url) {
       clippingPlanes = new Cesium.ClippingPlaneCollection({
+        //裁剪平面数组
         planes: [
           new Cesium.ClippingPlane(
-            new Cesium.Cartesian3(0.0, 0.0, -1.0),
+            new Cesium.Cartesian3(0.0, -1.0, 0.0),
             0.0
-          ),
+          ), //笛卡尔坐标系 平面的方向 以及 平面到原点的距离
         ],
         edgeWidth: 10.0,//剪裁对象的边的高光的宽度（以像素为单位）。
       });
 
       tileset = viewer.scene.primitives.add(
         new Cesium.Cesium3DTileset({
-          url: url,
+          url: url,//josn地址
           clippingPlanes: clippingPlanes,
-          debugShowBoundingVolume:false,// 会渲染模型的边界
+          debugShowBoundingVolume: false,// 会渲染模型的边界
         })
       );
-
+      //模型加载完成之后
       return tileset.readyPromise
         .then(function () {
           const boundingSphere = tileset.boundingSphere;//获取模型的空间坐标系的中心点(xyz)  与 半径
-          
+
           const radius = boundingSphere.radius;
 
           viewer.zoomTo(
             tileset,
             new Cesium.HeadingPitchRange(0.5, -0.5, radius * 4.0)// 参数一 0-6约等于一圈  参数二 俯视角度 负数往上  参数三 到中心点的距离(米)
-          );
+          );//移动摄像机到模型的对应位置
 
-        
+
 
           if (
             !Cesium.Matrix4.equals(
@@ -98,17 +99,16 @@ export default {
               new Cesium.Cartesian3(0.0, 0.0, height)
             );
           }
-            console.log(clippingPlanes,'值');
           for (let i = 0; i < clippingPlanes.length; ++i) {
             const plane = clippingPlanes.get(i);
-            console.log(plane,'789789789789','第'+i+'个');
+            //添加裁剪平面
             const planeEntity = viewer.entities.add({
               position: boundingSphere.center,
               plane: {
-                  //切面的矩形大小
+                //切面的矩形大小
                 dimensions: new Cesium.Cartesian2(
-                  radius * 2.5,
-                  radius * 2.5
+                  radius * 2.5,//模型半径的2.5倍
+                  radius * 2.5//模型半径的2.5倍
                 ),
                 material: Cesium.Color.RED.withAlpha(0.1),//切面的颜色
                 plane: new Cesium.CallbackProperty(
@@ -130,10 +130,11 @@ export default {
 
     const downHandler = new Cesium.ScreenSpaceEventHandler(
       viewer.scene.canvas
-    );
+    );//获取地图对象
+    //鼠标左键按下事件
     downHandler.setInputAction(function (movement) {
       const pickedObject = scene.pick(movement.position);
-      console.log(pickedObject,'123123');
+      console.log(pickedObject, '123123');
       if (
         Cesium.defined(pickedObject) &&
         Cesium.defined(pickedObject.id) &&
@@ -150,6 +151,7 @@ export default {
     const upHandler = new Cesium.ScreenSpaceEventHandler(
       viewer.scene.canvas
     );
+    //鼠标左键抬起事件
     upHandler.setInputAction(function () {
       if (Cesium.defined(selectedPlane)) {
         selectedPlane.material = Cesium.Color.WHITE.withAlpha(0.1);//停止时的截面颜色
@@ -164,10 +166,11 @@ export default {
     const moveHandler = new Cesium.ScreenSpaceEventHandler(
       viewer.scene.canvas
     );
+    // 鼠标移动事件
     moveHandler.setInputAction(function (movement) {
-        console.log(movement,'我移动了',Cesium.defined(selectedPlane));
+      //movement 鼠标移动的初始xy位置和移动后的xy位置   ,屏幕坐标
       if (Cesium.defined(selectedPlane)) {
-        const deltaY = movement.startPosition.y - movement.endPosition.y;
+        const deltaY = movement.startPosition.y - movement.endPosition.y;//计算开始和结束位置 重新赋值
         targetY += deltaY;
       }
     }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
@@ -175,18 +178,30 @@ export default {
     function createPlaneUpdateFunction(plane) {
       return function () {
         plane.distance = targetY;
-        // console.log(plane,'这是啥玩意');
         return plane;
       };
     }
 
 
+    loadTileset('/apies/SampleData/Cesium3DTiles/Instanced/InstancedOrientation/tileset.json');
 
-
-    loadTileset('/apies/SampleData/Cesium3DTiles/Instanced/InstancedOrientation/tileset.json')
-
-
-
+    // Cesium.ScreenSpaceEventType.LEFT_CLICK //鼠标左击事件
+    // Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK //鼠标左键双击事件
+    // Cesium.ScreenSpaceEventType.LEFT_DOWN //左键鼠标按下事件
+    // Cesium.ScreenSpaceEventType.LEFT_UP //左键鼠标抬起事件
+    // Cesium.ScreenSpaceEventType.MIDDLE_CLICK //中键单机
+    // Cesium.ScreenSpaceEventType.MIDDLE_DOWN //中键按下
+    // Cesium.ScreenSpaceEventType.MIDDLE_UP //中键抬起
+    // Cesium.ScreenSpaceEventType.MOUSE_MOVE //鼠标移动
+    // Cesium.ScreenSpaceEventType.PINCH_END //表示两指事件在触摸面上的结束。
+    // Cesium.ScreenSpaceEventType.PINCH_MOVE //两指移动
+    // Cesium.ScreenSpaceEventType.PINCH_START //表示在触摸面上发生两指事件的开始。
+    // Cesium.ScreenSpaceEventType.RIGHT_CLICK //鼠标右击事件
+    // Cesium.ScreenSpaceEventType.RIGHT_DOWN  //鼠标右键按下
+    // Cesium.ScreenSpaceEventType.RIGHT_UP    //鼠标右键抬起
+    // Cesium.ScreenSpaceEventType.WHEEL //鼠标滚轮事件
+    //let handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);获取地图对象
+    //handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK)//移除事件
 
   },
   methods: {
